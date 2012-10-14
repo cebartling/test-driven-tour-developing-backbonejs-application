a3ec25f6f78bd44eb4a70679c86165f3c5df134c
Christopher Bartling <chris.bartling@gmail.com>
2012-10-13 22:47:41 -0700

    Initial commit after creating the Rails application.

---
+++ rails-app/issue-tracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IssueTracker::Application.config.secret_token = '8aa7eb94ebee43edc3d50d340b0df7c5416ea9b8827514ab8091c0d4aa66365fe589d5e596e7915a4822db7fbae6b95743b55981e947b8d0a9628e198635a398'
